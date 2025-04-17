04fb957f1ee1bdbdf4fd23e4cffde9bd6745810b
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-04-17 15:51:11 -0300

    Add more egg blocks and add cutout renderer

--- Fabric/src/main/java/com/cerbon/adorable_eggs/fabric/AdorableEggsFabric.java
+++ Fabric/src/main/java/com/cerbon/adorable_eggs/fabric/AdorableEggsFabric.java
@@ -1,8 +1,11 @@
 package com.cerbon.adorable_eggs.fabric;
 
 import com.cerbon.adorable_eggs.AdorableEggs;
+import com.cerbon.adorable_eggs.block.ADEBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 
 public class AdorableEggsFabric implements ModInitializer, ClientModInitializer {
 
@@ -12,5 +15,7 @@ public class AdorableEggsFabric implements ModInitializer, ClientModInitializer 
     }
 
     @Override
-    public void onInitializeClient() {}
+    public void onInitializeClient() {
+        ADEBlocks.BLOCKS.getEntries().forEach(block -> BlockRenderLayerMap.INSTANCE.putBlock(block.get(), RenderType.cutout()));
+    }
 }
