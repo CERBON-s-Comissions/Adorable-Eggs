23b206466bd47d54f4702b1cff3f23fce7ae211c
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2026-03-24 21:33:28 -0300

    Add compatibility with apothic spawners

--- Common/src/main/java/com/cerbon/adorable_eggs/mixin/SpawnEggItemMixin.java
+++ Common/src/main/java/com/cerbon/adorable_eggs/mixin/SpawnEggItemMixin.java
@@ -2,6 +2,7 @@ package com.cerbon.adorable_eggs.mixin;
 
 import com.cerbon.adorable_eggs.AdorableEggs;
 import com.cerbon.adorable_eggs.block.custom.EggBlock;
+import com.cerbon.cerbons_api.api.static_utilities.MiscUtils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.advancements.CriteriaTriggers;
@@ -61,9 +62,14 @@ public class SpawnEggItemMixin extends Item {
 
     @Inject(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBlockEntity(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;", ordinal = 0), cancellable = true)
     private void useOnSpawner(UseOnContext context, CallbackInfoReturnable<InteractionResult> cir) {
-        Player player = context.getPlayer();
-        if (player != null && !player.isCreative() && context.getLevel().getBlockState(context.getClickedPos()).is(Blocks.SPAWNER))
-            cir.setReturnValue(InteractionResult.FAIL);
+        if (!MiscUtils.isModLoaded("apothic_spawners")) {
+            Player player = context.getPlayer();
+            BlockState blockState = context.getLevel().getBlockState(context.getClickedPos());
+            boolean isSpawner = blockState.is(Blocks.SPAWNER);
+
+            if (player != null && !player.isCreative() && isSpawner)
+                cir.setReturnValue(InteractionResult.FAIL);
+        }
     }
 
     @Inject(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/shapes/VoxelShape;"), cancellable = true)
